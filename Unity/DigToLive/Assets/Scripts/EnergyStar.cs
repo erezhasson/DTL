4b8de25c431262dcc181a02c30bf54562efaac9f
erez32 <69516798+erez32@users.noreply.github.com>
2021-02-05 12:03:03 +0200

    Created Ruler & Scripts

---
+++ Unity/DigToLive/Assets/Scripts/EnergyStar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyStar : MonoBehaviour
+{
+     public Transform m_StarTrans;
+     public int m_Size;
+     private float starRadius = 10;
+
+     // Start is called before the first frame update
+     void Start()
+     {
+
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+          drawStar();
+     }
+
+     private void drawStar()
+     {
+          m_StarTrans.localScale = new Vector3(m_Size + starRadius * 2, m_Size + starRadius * 2, m_Size + starRadius * 2);
+     }
+}
