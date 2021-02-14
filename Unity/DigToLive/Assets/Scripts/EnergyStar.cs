b667ca9720a8561833fa55251eacd446270a935f
erez32 <69516798+erez32@users.noreply.github.com>
2021-02-14 16:25:57 +0200

    Added Energy Star

--- Unity/DigToLive/Assets/Scripts/EnergyStar.cs
+++ Unity/DigToLive/Assets/Scripts/EnergyStar.cs
@@ -4,24 +4,107 @@ using UnityEngine;
 
 public class EnergyStar : MonoBehaviour
 {
-     public Transform m_StarTrans;
-     public int m_Size;
-     private float starRadius = 10;
+     private ParticleSystem.ShapeModule energyShape;
+     public float m_logicalSize, m_physicalSize;
+     private bool m_isExpanding;
+     private const float radiusFctr = 5;
+
+     public float StarSize
+     {
+          get
+          {
+               return m_logicalSize;
+          }
+
+          set
+          {
+               m_logicalSize = value;
+          }
+     }
 
      // Start is called before the first frame update
      void Start()
      {
-
+          energyShape = GetComponent<ParticleSystem>().shape;
      }
 
      // Update is called once per frame
      void Update()
      {
-          drawStar();
+          if (m_physicalSize >= 10 * radiusFctr)
+          {
+               explode();
+          }
+
+          else if (m_physicalSize <= radiusFctr && m_logicalSize <= radiusFctr)
+          {
+               shrink();
+          }
+
+          if (m_physicalSize != m_logicalSize)
+          {
+               resizeStar(m_logicalSize > m_physicalSize);
+          }
+
+          if (m_physicalSize == 20f)
+          {
+               m_logicalSize = 0;
+          }
+
+     }
+     /*
+     public void btnGet_Status_Clicked()
+     {
+          StartCoroutine(ServerGateway.HttpPostWrapper(p_Controller: "DTLGame",
+              p_OP: "Get_Status",
+              p_jsnInData: "", OnDTLGame_Get_Status_Complete));
+
+     }
+     public void OnDTLGame_Get_Status_Complete(Newtonsoft.Json.Linq.JToken p_jsnOutData, string p_jsnError)
+     {
+          if (p_jsnError != null)
+          {
+          }
+
+          else
+          {
+               m_logicalSize = float.Parse((string)p_jsnOutData["EnergyStarSize"]);
+          }
+
+     }
+     */
+     private void resizeStar(bool isExpanding)
+     {
+          float resizeFctr;
+
+          if (isExpanding)
+          {
+               resizeFctr = m_physicalSize <= m_logicalSize - 0.01f ? 0.01f : m_logicalSize - m_physicalSize;
+          }
+
+          else
+          {
+               resizeFctr = m_physicalSize >= m_logicalSize + 0.01f ? -0.01f : m_physicalSize - m_logicalSize;
+          }
+
+          energyShape.radius += resizeFctr;
+          m_physicalSize = energyShape.radius;
+          Debug.Log(m_physicalSize);
      }
 
-     private void drawStar()
+     private void explode()
      {
-          m_StarTrans.localScale = new Vector3(m_Size + starRadius * 2, m_Size + starRadius * 2, m_Size + starRadius * 2);
+          
+     }
+
+     private void shrink()
+     {
+          while (m_physicalSize >= 0)
+          {
+               m_physicalSize = m_physicalSize >= 0.05f ? m_physicalSize - 0.05f : 0;
+               energyShape.radius = m_physicalSize;
+          }
+
+          m_physicalSize = 0;
      }
 }
---
+++ Unity/DigToLive/Assets/Scripts/ServerGateaway.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public static class ServerGateway
+{
+     const int HttpStatusCodeOK = (int)System.Net.HttpStatusCode.OK;
+
+     const string c_Baseuri = "http://localhost:55239";
+     //const string c_Baseuri = "http://digtolive.hopto.org:55239";
+
+     public static IEnumerator HttpPostWrapper(string p_Controller,
+                string p_OP, string p_jsnInData,
+               System.Action<Newtonsoft.Json.Linq.JToken, string> callbackOnFinish)
+     {
+          WWWForm form = new WWWForm();
+          form.AddField("op", p_OP);
+          form.AddField("jsnInData", p_jsnInData);
+
+          UnityWebRequest www = UnityWebRequest.Post(c_Baseuri + "/" + p_Controller, form);
+          yield return www.SendWebRequest();
+
+          if (www.isNetworkError || www.isHttpError)
+          {
+               callbackOnFinish(null, www.error);
+
+          }
+          else
+          {
+               string ServerResponseData = www.downloadHandler.text;
+
+               Newtonsoft.Json.Linq.JContainer OServerData =
+               Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(ServerResponseData);
+               string StatusCode = (string)OServerData["StatusCode"];
+               if (StatusCode != HttpStatusCodeOK.ToString())
+               {
+                    string p_jsnError = (string)OServerData["ErrorDescription"];
+                    callbackOnFinish(null, p_jsnError);
+               }
+               else
+               {
+                    Newtonsoft.Json.Linq.JToken jsnDataOut = OServerData["jsnDataOut"];
+                    callbackOnFinish(jsnDataOut, null);
+               }
+          }
+
+     }
+}
