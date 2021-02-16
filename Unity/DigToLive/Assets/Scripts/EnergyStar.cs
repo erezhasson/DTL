d585fa72c24159029fb7386797751b4d352738e4
erez32 <69516798+erez32@users.noreply.github.com>
2021-02-16 13:43:53 +0200

    Server Intergration

--- Unity/DigToLive/Assets/Scripts/EnergyStar.cs
+++ Unity/DigToLive/Assets/Scripts/EnergyStar.cs
@@ -4,107 +4,91 @@ using UnityEngine;
 
 public class EnergyStar : MonoBehaviour
 {
+     private ParticleSystem m_StarPS;
      private ParticleSystem.ShapeModule energyShape;
-     public float m_logicalSize, m_physicalSize;
-     private bool m_isExpanding;
-     private const float radiusFctr = 5;
+     public float m_lastSize, m_Size = radiusFctr;
+     private const float radiusFctr = 5, MaxSize = 10 * radiusFctr;
 
      public float StarSize
      {
           get
           {
-               return m_logicalSize;
+               return m_Size;
           }
 
           set
           {
-               m_logicalSize = value;
+               m_Size = (value + 1) * radiusFctr;
+          }
+     }
+
+     public float LogicalStarSize
+     {
+          get
+          {
+               return m_Size;
           }
      }
 
      // Start is called before the first frame update
      void Start()
      {
-          energyShape = GetComponent<ParticleSystem>().shape;
+          m_StarPS = GetComponent<ParticleSystem>();
+          energyShape = m_StarPS.shape;
      }
 
      // Update is called once per frame
      void Update()
      {
-          if (m_physicalSize >= 10 * radiusFctr)
+          if (OutOfRange())
           {
-               explode();
+               if (m_Size > radiusFctr)
+               {
+                    explode();
+               }
+
+               else
+               {
+                    shrink();
+               }
+
+               m_Size = m_lastSize;
           }
 
-          else if (m_physicalSize <= radiusFctr && m_logicalSize <= radiusFctr)
-          {
-               shrink();
-          }
-
-          if (m_physicalSize != m_logicalSize)
-          {
-               resizeStar(m_logicalSize > m_physicalSize);
-          }
-
-          if (m_physicalSize == 20f)
-          {
-               m_logicalSize = 0;
-          }
-
+          energyShape.radius = m_Size;
+          Debug.Log(m_Size);
      }
-     /*
-     public void btnGet_Status_Clicked()
-     {
-          StartCoroutine(ServerGateway.HttpPostWrapper(p_Controller: "DTLGame",
-              p_OP: "Get_Status",
-              p_jsnInData: "", OnDTLGame_Get_Status_Complete));
 
+     private float getStarSize()
+     {
+          return Random.Range(0f, 100f);
      }
-     public void OnDTLGame_Get_Status_Complete(Newtonsoft.Json.Linq.JToken p_jsnOutData, string p_jsnError)
+
+     private bool OutOfRange()
      {
-          if (p_jsnError != null)
-          {
-          }
-
-          else
-          {
-               m_logicalSize = float.Parse((string)p_jsnOutData["EnergyStarSize"]);
-          }
-
-     }
-     */
-     private void resizeStar(bool isExpanding)
-     {
-          float resizeFctr;
-
-          if (isExpanding)
-          {
-               resizeFctr = m_physicalSize <= m_logicalSize - 0.01f ? 0.01f : m_logicalSize - m_physicalSize;
-          }
-
-          else
-          {
-               resizeFctr = m_physicalSize >= m_logicalSize + 0.01f ? -0.01f : m_physicalSize - m_logicalSize;
-          }
-
-          energyShape.radius += resizeFctr;
-          m_physicalSize = energyShape.radius;
-          Debug.Log(m_physicalSize);
+          return m_Size > MaxSize || m_Size < radiusFctr;
      }
 
      private void explode()
      {
-          
+          while (m_StarPS.startSpeed < 20)
+          {
+               m_StarPS.startSpeed += 0.5f;
+          }
+
+          while (m_StarPS.startSpeed > 0)
+          {
+               m_StarPS.startSpeed -= 0.5f;
+          }
      }
 
      private void shrink()
      {
-          while (m_physicalSize >= 0)
+          while (m_Size >= 0)
           {
-               m_physicalSize = m_physicalSize >= 0.05f ? m_physicalSize - 0.05f : 0;
-               energyShape.radius = m_physicalSize;
+               m_Size = m_Size >= 0.001f ? m_Size - 0.001f : 0;
           }
 
-          m_physicalSize = 0;
+          GetComponent<Renderer>().enabled = false;
      }
 }
